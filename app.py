6df66ab854d7d167661d50e5c7f01af8b139d5a2
maddires-del <maddires@mail.gvsu.edu>
2026-06-14 20:28:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,25 @@ Resume box
 Job Description box
 User Goal box
 Run Agent button
+def skill_gap_tool(resume):
+    skills = [
+        "Python",
+        "SQL",
+        "Machine Learning",
+        "Statistics",
+        "Excel",
+        "Tableau",
+        "Power BI",
+        "Pandas",
+        "Scikit-learn",
+        "GitHub",
+        "Generative AI"
+    ]
+
+    missing = []
+
+    for skill in skills:
+        if skill.lower() not in resume.lower():
+            missing.append(skill)
+
+    return missing
